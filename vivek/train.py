e6e87e344628f4185fa4248bc71656d9d00e4ce3
vivekramkumar <41638630+vivekramkumar@users.noreply.github.com>
2021-07-31 10:09:32 +0530

    Update train.py

--- vivek/train.py
+++ vivek/train.py
@@ -7,8 +7,8 @@ from cleaned import clean
 import pickle
 import final_similar
 warnings.filterwarnings("ignore")
-df = pd.read_csv('E:\\intern\\threads1.csv')
-incident= pd.read_csv('E:\\intern\\Incident.csv')
+df = pd.read_csv('//home//ec2-user//intern//intern-test//data//threads1.csv')
+incident= pd.read_csv('//home//ec2-user//intern//intern-test//data//Incident.csv')
 df=clean(df,incident) #clean the dataset
 
 
@@ -20,16 +20,16 @@ for doc in tqdm(nlp.pipe(df.Text.iloc[:].str.lower().values, disable=["tagger", 
     tok = [t.text for t in doc if t.is_alpha]
     tok_text.append(tok)
 #write tokens in csv file
-with open("E:\\intern\\final_full_length.csv", "w", newline="",encoding="utf-8") as f:
+with open("//home//ec2-user//intern//intern-test//final_full_length.csv", "w", newline="",encoding="utf-8") as f:
     writer = csv.writer(f)
     writer.writerows(tok_text)
     
-with open('E:\\intern\\final_full_length.csv', newline='',encoding="utf-8") as f:
+with open('//home//ec2-user//intern//intern-test//final_full_length.csv', newline='',encoding="utf-8") as f:
     reader = csv.reader(f)
     data = list(reader)
 #create bmi class object after reading from csv file
 bmi=final_similar.find_similar_api(data) 
-with open('E:\\intern\\object.obj', 'wb') as object_file:
+with open('//home//ec2-user//intern//intern-test//object.obj', 'wb') as object_file:
  
   pickle.dump(bmi, object_file)
 print("Training done!!")
